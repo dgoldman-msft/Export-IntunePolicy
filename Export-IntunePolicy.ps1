a95fd6970c0116eb14bc63165c5889f3f2579f0d
Dave Goldman <39909564+dgoldman-msft@users.noreply.github.com>
2022-10-22 19:00:11 -0400

    Initial Checkin

---
+++ Export-IntunePolicy.ps1
@@ -0,0 +1,223 @@
+﻿function Export-IntunePolicy {
+    <#
+        .SYNOPSIS
+            Export Intune Policies
+
+        .DESCRIPTION
+            Connect using Graph API and export Intune policies of choice
+
+        .PARAMETER LoggingPath
+            Logging path
+
+        .PARAMETER ResourceType
+            Graph namespace to retrieve
+
+        .PARAMETER SaveResultsToCSV
+            Save results to disk in CSV format
+
+        .PARAMETER SaveResultsToJSON
+            Save results to disk in JSON format
+
+        .PARAMETER ShowModuleInfoInVerbose
+            Used to troubleshoot module install and import
+
+        .PARAMETER ShowFull
+            Save results to disk
+
+        .EXAMPLE
+            PS C:\Export-IntunePolicy -ResourceType configurationPolicies
+
+            Retrieves Intune configurationPolicies and displays them in a limited custom formatted view
+
+        .EXAMPLE
+            PS C:\Export-IntunePolicy -ResourceType configurationPolicies -ShowFull
+
+            Retrieves Intune configurationPolicies and displays them with all policy details to the console
+
+        .EXAMPLE
+            PS C:\Export-IntunePolicy -ResourceType configurationPolicies -SaveResultsToCSV
+
+            Retrieves Intune configurationPolicies and saves the policies in csv format
+
+        .EXAMPLE
+            PS C:\Export-IntunePolicy -ResourceType configurationPolicies -SaveResultsToJSON
+
+            Retrieves Intune configurationPolicies and saves the policies in json format
+
+        .NOTES
+            https://learn.microsoft.com/en-us/powershell/microsoftgraph/get-started?view=graph-powershell-1.0
+   #>
+
+    [OutputType('PSCustomObject')]
+    [CmdletBinding()]
+    [Alias('ExportIP')]
+    param(
+        [string]
+        $LoggingPath = "$env:Temp\ExportedIntunePolicies",
+
+        [ValidateSet('configurationPolicies', 'deviceCompliancePolicies', 'deviceConfigurations', 'deviceEnrollmentConfigurations', `
+                'deviceEnrollmentPlatformRestriction', 'defaultManagedAppProtections', `
+                'mdmWindowsInformationProtectionPolicies', 'iosManagedAppProtections', 'managedAppPolicies' )]
+        [string]
+        $ResourceType = "deviceCompliancePolicies",
+
+        [switch]
+        $SaveResultsToCSV,
+
+        [switch]
+        $SaveResultsToJSON,
+
+        [switch]
+        $ShowModuleInfoInVerbose,
+
+        [switch]
+        $ShowFull
+    )
+
+    begin {
+        Write-Output "Collecting Intune Configuration Profiles"
+        $parameters = $PSBoundParameters
+        [System.Collections.ArrayList]$configurationPolicies = @()
+        $modules = @("Microsoft.Graph.Authentication", "Microsoft.Graph.Intune")
+        $successful = $false
+
+        try {
+            if (-NOT(Test-Path -Path $LoggingPath)) {
+                if (New-Item -Path $LoggingPath -ItemType Directory -ErrorAction Stop) {
+                    Write-Verbose "$LoggingPath directory created!"
+                }
+            }
+            Write-Verbose "$LoggingPath directory already exists!"
+        }
+        catch {
+            Write-Output "Error: $_"
+        }
+    }
+
+    process {
+        try {
+            foreach ($module in $modules) {
+                if ($found = Get-Module -Name $module -ListAvailable | Sort-Object Version | Select-Object -First 1) {
+                    if (Import-Module -Name $found -ErrorAction Stop -PassThru -Verbose:$ShowModuleInfoInVerbose) {
+                        Write-Verbose "$found imported!"
+                        $successful = $true
+                    }
+                    else {
+                        Write-Verbose "Error importing $found"
+                        $successful = $false
+                    }
+                }
+                else {
+                    Write-Output "$module not found! Installing module $($module) from the PowerShell Gallery"
+                    if (Install-Module -Name $module -Repository PSGallery -Force -PassThru -Verbose:$ShowModuleInfoInVerbose) {
+                        Write-Verbose "$module installed successfully! Importing $($module)"
+                        if (Import-Module -Name $module -ErrorAction Stop -PassThru -Verbose:$ShowModuleInfoInVerbose) {
+                            Write-Verbose "$module imported successfully!"
+                            $successful = $true
+                        }
+                        else {
+                            Write-Verbose "Error importing $found"
+                            $successful = $false
+                        }
+                    }
+                }
+            }
+
+            if ($successful) {
+                Select-MgProfile -Name "beta" -ErrorAction Stop
+                Connect-MgGraph -Scopes "User.Read.All", "DeviceManagementApps.Read.All", "DeviceManagementConfiguration.Read.All", "DeviceManagementServiceConfig.Read.All" -ForceRefresh -ErrorAction Stop
+                Write-Output "Connected to the Graph endpoint"
+            }
+            else {
+                Write-Output "Error: Unable to connect to the Graph endpoint"
+                return
+            }
+        }
+        catch {
+            Write-Output "Error: $_"
+        }
+
+        try {
+            if (($ResourceType -eq 'iosManagedAppProtections') -or ($ResourceType -eq 'managedAppPolicies')`
+                    -or ($ResourceType -eq 'defaultManagedAppProtections') -or ($ResourceType -eq 'mdmWindowsInformationProtectionPolicies')) {
+                $uri = "https://graph.microsoft.com/beta/deviceAppManagement/$ResourceType"
+            }
+            else {
+                $uri = "https://graph.microsoft.com/beta/deviceManagement/$ResourceType"
+            }
+
+            Write-Verbose "Connecting to the Graph API endpoint: $($uri)"
+            if ($policies = Invoke-MgGraphRequest -Method GET -Uri $uri) {
+                foreach ($policy in $policies.value) {
+                    $policyFound = [PSCustomObject]@{ PSTypeName = "Intune $ResourceType" }
+                    foreach ($policyItem in $policy.GetEnumerator()) {
+                        if (($policyItem.Key -eq 'validOperatingSystemBuildRanges') -or ($policyItem.Key -eq 'roleScopeTagIds')) {
+                            $policyFound | Add-Member -MemberType NoteProperty -Name $policyItem.key -Value ($policyItem.Value -Join ',')
+                        }
+                        else {
+                            $policyFound | Add-Member -MemberType NoteProperty -Name $policyItem.key -Value $policyItem.value
+                        }
+                    }
+                    $null = $configurationPolicies.add($policyFound)
+                }
+            }
+            else {
+                Write-Output "No Graph results returned!"
+            }
+        }
+        catch {
+            Write-Output "Error: $_"
+        }
+
+        try {
+            if ($parameters.ContainsKey('ShowFull')) {
+                [PSCustomObject]$configurationPolicies
+            }
+            else {
+                if ($ResourceType -eq 'configurationPolicies') {
+                    $TypeData = @{
+                        TypeName                  = "Intune $ResourceType"
+                        DefaultDisplayPropertySet = 'name', 'id', 'createdDateTime', 'lastModifiedDateTime'
+                    }
+                    Update-TypeData @TypeData
+                    [PSCustomObject]$configurationPolicies
+                    Remove-TypeData -TypeName "Intune $ResourceType"
+                }
+                if (($ResourceType -eq 'deviceCompliancePolicies') -or
+                ($ResourceType -eq 'deviceConfigurations') -or
+                (($ResourceType -eq 'deviceEnrollmentConfigurations'))) {
+                    $TypeData = @{
+                        TypeName                  = "Intune $ResourceType"
+                        DefaultDisplayPropertySet = 'displayName', 'id', 'createdDateTime', 'lastModifiedDateTime'
+                    }
+                    Update-TypeData @TypeData
+                    [PSCustomObject]$configurationPolicies
+                    Remove-TypeData -TypeName "Intune $ResourceType"
+                }
+            }
+
+            if ($parameters.ContainsKey('SaveResultsToCSV')) {
+                foreach ($policy in $configurationPolicies) {
+                    Write-Verbose "Saving $($policy.displayName + ".csv")"
+                    [PSCustomObject]$policy | Export-Csv -Path (Join-Path -Path $LoggingPath -ChildPath $($policy.displayName + ".csv")) -ErrorAction Stop -Encoding UTF8 -NoTypeInformation -Append
+                }
+            }
+
+            if ($parameters.ContainsKey('SaveResultsToJSON')) {
+                foreach ($policy in $configurationPolicies) {
+                    Write-Verbose "Saving $($policy.displayName + ".json")"
+                    [PSCustomObject]$policy | ConvertTo-Json -Depth 10 | Set-Content (Join-Path -Path $LoggingPath -ChildPath $($policy.displayName + ".json")) -ErrorAction Stop -Encoding UTF8
+                }
+            }
+        }
+        catch {
+            Write-Output "Error: $_"
+        }
+    }
+
+    end {
+        if (($parameters.ContainsKey('SaveResultsToCSV')) -or ($parameters.ContainsKey('SaveResultsToJSON'))) {
+            Write-Output "`nResults exported to: $($LoggingPath)`nCompleted!"
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # Export-IntunePolicy
 Exports a number of Intune policies in csv and json format
+
+## Getting Started with Export-IntunePolicy
+
+Running this script you agree to install Microsoft.Graph PowerShell modules and consent to permissions on your system so you can connect to GraphAPI to export Intune policy information
+
+### DESCRIPTION
+
+Connect using Graph API (Beta) and export Intune policies of choice. As of now these are the items that can be exported:
+
+    configurationPolicies
+    deviceCompliancePolicies
+    deviceConfigurations
+    deviceEnrollmentConfigurations
+    deviceEnrollmentPlatformRestriction
+    defaultManagedAppProtections
+    mdmWindowsInformationProtectionPolicies
+    iosManagedAppProtections
+    managedAppPolicies
+
+- EXAMPLE 1: PS C:\Export-IntunePolicy -ResourceType configurationPolicies
+
+    Retrieves Intune configurationPolicies and displays them in a limited custom formatted view
+
+- EXAMPLE 2: PS C:\Export-IntunePolicy -ResourceType configurationPolicies -ShowFull
+
+    Retrieves Intune configurationPolicies and displays them with all policy details to the console
+
+- EXAMPLE 3: PS C:\Export-IntunePolicy -ResourceType configurationPolicies -SaveResultsToCSV
+
+    Retrieves Intune configurationPolicies and saves the policies in csv format
+
+- EXAMPLE 4: PS C:\Export-IntunePolicy -ResourceType configurationPolicies -SaveResultsToJSON
+
+    Retrieves Intune configurationPolicies and saves the policies in json format
+
+### Note on file export
+
+All policies will be exported in csv or json to "$env:Temp\ExportedIntunePolicies". This path can be changed if necessary.
